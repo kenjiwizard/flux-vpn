de9129cd2d35aea8b90b5c2ee0a47a9c4eeb5236
Kenji <120318704+kenjiwizard@users.noreply.github.com>
2026-03-31 00:34:07 +0300

    Update postinstall.sh

--- dist-assets/pkg-scripts/postinstall.sh
+++ dist-assets/pkg-scripts/postinstall.sh
@@ -2,4 +2,4 @@
 # Post-installation script for macOS
 # Starts the Flux VPN daemon after installation
 
-launchctl load -w /Library/LaunchDaemons/org.fluxvpn.daemon.plist
+launchctl load -a -w /Library/LaunchDaemons/org.fluxvpn.daemon.plist
